e8cd038f32e7d80b3513d279d0021f870739ea8c
Franco <frcg2424@hotmail.com>
2022-08-13 18:15:08 -0500

    feat: :sparkles: add border countries navigation

--- src/components/BorderCountry.jsx
+++ src/components/BorderCountry.jsx
@@ -1,21 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { getCountrieByCode } from '../services/CountryService';
 
 const BorderCountry = ({ border }) => {
 
+    const navigate = useNavigate();
     const [countryName, setCountryName] = useState("");
 
     useEffect(() => {
         getCountrieByCode(border)
             .then((response)=>{
-                setCountryName(response.data[0].nativeName)
+                setCountryName(response.data[0])
             })
 
     }, [border]);
 
     return (
-        <div>
-            { countryName }
+        <div onClick={ ()=> { navigate(`../country/${countryName.name}`, { replace: false }); } }>
+            { countryName?.translations?.es }
         </div>
     );
 }
